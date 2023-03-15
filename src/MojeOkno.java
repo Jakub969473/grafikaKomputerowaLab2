81b5076a427fc60cbcf02e85c6061e4d0ef62ac6
Jakub969473 <124714550+Jakub969473@users.noreply.github.com>
2023-03-15 20:17:59 +0100

    I created panels for two input images and one output image

--- src/MojeOkno.java
+++ src/MojeOkno.java
@@ -13,8 +13,10 @@ import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 public class MojeOkno  extends JFrame implements ActionListener {
     //Tworzymy panele okna głównego
-    PanelGraficzny lewy = new PanelGraficzny();
-    PanelGraficzny prawy = new PanelGraficzny();
+    PanelGraficzny wejscie1 = new PanelGraficzny();
+    PanelGraficzny wejscie2 = new PanelGraficzny();
+    PanelGraficzny wyjscie = new PanelGraficzny();
+
     //Tworzymy menu okna
     MenuOkna menu = new MenuOkna();
     //scieżka dostępu wraz z nazwą pliku
@@ -35,8 +37,9 @@ public class MojeOkno  extends JFrame implements ActionListener {
         //ustawienie stworzonego menu
         setJMenuBar(menu);
         //dodanie paneli
-        add(lewy);
-        add(prawy);
+        add(wejscie1);
+        add(wejscie2);
+        add(wyjscie);
         //przypisanie obsługi akcji
         ustawNasluchZdarzen();
         dopasujSieDoZawartosci();
@@ -50,22 +53,6 @@ public class MojeOkno  extends JFrame implements ActionListener {
         menu.zapiszPlik.addActionListener(this);
         menu.zakoncz.addActionListener(this);
 
-        menu.lewCzysc.addActionListener(this);
-        menu.lewZad1.addActionListener(this);
-
-        menu.praCzysc.addActionListener(this);
-
-        menu.czerwony.addActionListener(this);
-        menu.zielony.addActionListener(this);
-        menu.niebieski.addActionListener(this);
-        menu.srednia.addActionListener(this);
-        menu.YUV.addActionListener(this);
-
-
-        menu.jasnoc.addActionListener(this);
-        menu.kontrast.addActionListener(this);
-        menu.negacja.addActionListener(this);
-        menu.zakres.addActionListener(this);
     }
 
     //METODY OBSŁUGI ZDARZEŃ DLA INTERFEJSÓW
@@ -81,143 +68,6 @@ public class MojeOkno  extends JFrame implements ActionListener {
             zapiszPlik();
         } else if (label.equals("Zakończ")) {
             System.exit(0);
-        } else if (label.equals("Wyczyść lewy")) {
-            lewy.wyczysc();
-        } else if (label.equals("Kopiuj")) {
-            int w = prawy.plotno.getWidth();
-            int h = prawy.plotno.getHeight();
-            prawy.czyPusty = false;
-            //skopiuj prawy panel na bazie lewego
-            lewy.kopiuj(prawy.plotno);
-            //dopasowanie zawartości w przypadku zmiany wymiarów
-            if (w != prawy.plotno.getWidth() || h != prawy.plotno.getHeight())
-                dopasujSieDoZawartosci();
-        } else if (label.equals("Wyczyść prawy")) {
-            prawy.czyPusty = true;
-            prawy.wyczysc();
-        } else if (label.equals("Zmiana jasności")) {
-
-            int w = prawy.plotno.getWidth();
-            int h = prawy.plotno.getHeight();
-
-            String input = JOptionPane.showInputDialog("Proszę podać zmianę jasnosci");
-
-            double jasnosc = 0;
-
-            try {
-                jasnosc = Double.parseDouble(input);
-            } catch (NumberFormatException wrongInput) {
-                JOptionPane.showMessageDialog(null, "Wartość musi być liczbą,",
-                        "Alert", JOptionPane.ERROR_MESSAGE);
-            }
-
-
-            prawy.jasność(lewy.plotno, jasnosc);
-            prawy.czyPusty = false;
-
-            if (w != prawy.plotno.getWidth() || h != prawy.plotno.getHeight())
-                dopasujSieDoZawartosci();
-
-        } else if (label.equals("Zmiana kontrastu")) {
-
-            int w = prawy.plotno.getWidth();
-            int h = prawy.plotno.getHeight();
-
-            String input = JOptionPane.showInputDialog("Proszę podać zmianę kotrastu");
-
-            double kontrast = 1;
-
-            try {
-                kontrast = Double.parseDouble(input);
-            } catch (NumberFormatException wrongInput) {
-                JOptionPane.showMessageDialog(null, "Wartość musi być liczbą,",
-                        "Alert", JOptionPane.ERROR_MESSAGE);
-            }
-
-
-            prawy.kontrast(lewy.plotno, kontrast);
-            prawy.czyPusty = false;
-
-            if (w != prawy.plotno.getWidth() || h != prawy.plotno.getHeight())
-                dopasujSieDoZawartosci();
-
-        } else if (label.equals("Negacja obrazu")) {
-
-            int w = prawy.plotno.getWidth();
-            int h = prawy.plotno.getHeight();
-
-
-            prawy.negacja(lewy.plotno);
-            prawy.czyPusty = false;
-
-            if (w != prawy.plotno.getWidth() || h != prawy.plotno.getHeight())
-                dopasujSieDoZawartosci();
-
-        } else if (label.equals("Zmiana zakresu jasnosci")) {
-
-            int w = prawy.plotno.getWidth();
-            int h = prawy.plotno.getHeight();
-
-
-            prawy.zmianaZakresuJasności(lewy.plotno);
-            prawy.czyPusty = false;
-
-            if (w != prawy.plotno.getWidth() || h != prawy.plotno.getHeight())
-                dopasujSieDoZawartosci();
-        } else if (label.equals("wg R")) {
-
-            int w = prawy.plotno.getWidth();
-            int h = prawy.plotno.getHeight();
-
-            prawy.szaryCzerwony(lewy.plotno);
-            prawy.czyPusty = false;
-
-            if (w != prawy.plotno.getWidth() || h != prawy.plotno.getHeight())
-                dopasujSieDoZawartosci();
-
-        } else if (label.equals("wg G")) {
-
-            int w = prawy.plotno.getWidth();
-            int h = prawy.plotno.getHeight();
-
-
-            prawy.szaryZieloy(lewy.plotno);
-            prawy.czyPusty = false;
-
-            if (w != prawy.plotno.getWidth() || h != prawy.plotno.getHeight())
-                dopasujSieDoZawartosci();
-
-        } else if (label.equals("wg B")) {
-
-            int w = prawy.plotno.getWidth();
-            int h = prawy.plotno.getHeight();
-
-            prawy.szaryNiebieski(lewy.plotno);
-            prawy.czyPusty = false;
-
-            if (w != prawy.plotno.getWidth() || h != prawy.plotno.getHeight())
-                dopasujSieDoZawartosci();
-
-        } else if (label.equals("średnia RGB")) {
-
-            int w = prawy.plotno.getWidth();
-            int h = prawy.plotno.getHeight();
-
-            prawy.szarysrednia(lewy.plotno);
-            prawy.czyPusty = false;
-
-            if (w != prawy.plotno.getWidth() || h != prawy.plotno.getHeight())
-                dopasujSieDoZawartosci();
-
-        } else if (label.equals("Model YUV")) {
-            int w = prawy.plotno.getWidth();
-            int h = prawy.plotno.getHeight();
-
-            prawy.modelYUV(lewy.plotno);
-            prawy.czyPusty = false;
-
-            if (w != prawy.plotno.getWidth() || h != prawy.plotno.getHeight())
-                dopasujSieDoZawartosci();
         }
     }
 
@@ -236,11 +86,11 @@ public class MojeOkno  extends JFrame implements ActionListener {
         if (wynik == JFileChooser.APPROVE_OPTION) {
             //wyłuskanie ścieżki do wybranego pliku
             sciezkaDoPlik = otworz.getSelectedFile().getPath();
-            int w = lewy.plotno.getWidth();
-            int h = lewy.plotno.getHeight();
+            int w = wejscie1.plotno.getWidth();
+            int h = wejscie1.plotno.getHeight();
             //wczytanie pliku graficznego na lewy panel
-            lewy.wczytajPlikGraficzny(sciezkaDoPlik);
-            if (w != lewy.plotno.getWidth() || h != lewy.plotno.getHeight())
+            wejscie1.wczytajPlikGraficzny(sciezkaDoPlik);
+            if (w != wejscie1.plotno.getWidth() || h != wejscie1.plotno.getHeight())
                 dopasujSieDoZawartosci();
         }
     }
@@ -265,7 +115,7 @@ public class MojeOkno  extends JFrame implements ActionListener {
             //wyłuskanie ścieżki do wybranego pliku
             sciezkaDoPlik = zapisz.getSelectedFile().getPath();
             //JOptionPane.showMessageDialog(null,"Blad odczytu pliku: " + sciezkaDoPlik);
-            prawy.zapiszPlikGraficzny(sciezkaDoPlik);
+            wyjscie.zapiszPlikGraficzny(sciezkaDoPlik);
         }
     }
 
